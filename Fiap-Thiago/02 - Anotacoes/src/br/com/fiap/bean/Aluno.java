e548afd0a6c4719218a962e0e8fbed61528c107b
André Kancelskis Azevedo de Oliveir <andre.oliveira@techne.com.br>
2020-02-17 17:29:33 -0300

    organizando arquivos

--- Fiap-Thiago/02 - Anotacoes/src/br/com/fiap/bean/Aluno.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.bean;
-
-import br.com.fiap.anotacao.Tabela;
-
-@Tabela(nome = "TB_ALUNO")
-public class Aluno {
-
-}
--- Fiap-Thiago/EX-revisao/src/br/com/fiap/bean/ContaInvestimento.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.bean;
-
-public interface ContaInvestimento {
-
-	public double calculaRetornoInvestimento();
-
-}
